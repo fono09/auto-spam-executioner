2b33364712b0879a69f8655f183d1d2d04e81266
fono09 <fono@fono.jp>
2024-02-20 08:26:50 +0900

    fono's patch

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# MastodonのStreamingAPIでリアルタイムでスパムを処すやつ
+
 ## License
 
 [AGPL License](https://opensource.org/licenses/AGPL).
@@ -8,6 +10,16 @@
 
 ## Original Code
 
+[S-H-GAMELINKS/auto-spam-executioner](https://github.com/S-H-GAMELINKS/auto-spam-executioner)
+
 [Reject spammer](https://github.com/best-friends/mastodon/pull/1941)
 
 [mastodon/mastodon](https://github.com/mastodon/mastodon)
+
+## 使い方
+
+APIキーの権限は下記があれば十分です
+(adminが必要なのでチェックを忘れないように。怖ければ適当に削ってください)
+`read, write, admin:read, admin:write`
+
+`cp .env.sample .env` として、`.env` を編集してください
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from mastodon import Mastodon, StreamListener
 from dotenv import load_dotenv
+from pprint import pprint
+from datetime import datetime, timezone, timedelta
+from time import sleep
 import os
-import pprint
 import time
 import urllib.error
 import urllib.request
-import datetime
+import asyncio
 
 # .envの読み込み
 load_dotenv()
+pprint(os.environ)
 
 # Mastodonのクライアント生成
 client = Mastodon(api_base_url = os.environ['API_BASE_URL'], access_token = os.environ['ACCESS_TOKEN'])
@@ -28,22 +31,52 @@ class PublicStreamListener(StreamListener):
             pass
 
     def on_update(self, status):
+        print(f"status recieved: {status.id}")
         try:
             # メンション数が規定数より多く、かつ外部からの投稿の場合
             if len(status.mentions) > int(os.environ['MEMTION_COUNT']) and status.account.acct.find('@') > 0:
                 statusID = status.id
                 account = status.account
+                pprint(status)
+
+                # 作成から7日以上経過したアカウントは対象としない
+                if account.created_at - datetime.now(timezone.utc) > timedelta(days=7):
+                    return
+
+                # フォロワー数を数え、規定数より多い場合は対象としない
+                if account.following_count > int(os.environ['FOLLOWER_COUNT']):
+                    return
 
                 # スパムの通報(リモートサーバにも転送)を投げ、アカウントを停止させる
-                report = self.client.report(account.id, status_ids=[statusID], forward=True, category='spam')
-                self.client.admin_account_moderate(account.id, 'suspend', report_id=report.id)
+                report = self.client.report(
+                    account.id,
+                    status_ids=[statusID],
+                    forward=True,
+                    category='spam'
+                )
+                pprint(report)
+
+                # レプリケーション遅延を考慮する
+                sleep(1)
+
+                # アカウントの停止
+                self.client.admin_account_moderate(
+                    account.id,
+                    action='suspend',
+                    report_id=report.id,
+                    send_email_notification=False
+                )
+
 
         except Exception as e:
-            print(e)
+            pprint(e)
             pass
 
 # 連合タイムラインのListenerを生成            
 public_stream_listener = PublicStreamListener(client)
 
 # 連合タイムラインをListen
-client.stream_public(public_stream_listener, remote=True)
+while True:
+    client.stream_public(public_stream_listener, remote=True)
+    print("restart litening client.stream_public")
+    sleep(10)
